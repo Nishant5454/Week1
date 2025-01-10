abe6ed4b8bfd62d763a32cd3993d390fc3ea22a4
Nishant kumar <bhagatnishant5454@gmail.com>
2025-01-10 11:36:34 +0530

    Delete Level1 directory

--- Level1/kmtoMiles.java
+++
@@ -1,8 +0,0 @@
-class kmtomiles{
-    public static void main(String[]args){
-    double distanceinKM=10.8;// totaldistance in the km given
-    double distanceinMiles=distanceinKM*1.6;// total distance in miles
-    System.out.println("The distance "+""+ distanceinKM+" "+"km in miles is "+ distanceinMiles);// output
-    }
-    }
-
