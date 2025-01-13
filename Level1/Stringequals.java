8b89dc6d343b63f1af95a37799861b06e14e058b
Nishant5454 <bhagatnishant5454@gmail.com>
2025-01-13 15:18:00 +0530

    aded the files

--- Level1/Stringequals.java
+++ Level1/Stringequals.java
@@ -1,24 +1,44 @@
+
 import java.util.Scanner;
 
-class Stringequals{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in); 
-    System.out.println("Enter first String"); 
-    String s1=sc.nextLine(); 
-    System.out.println("Enter the second String"); 
-    String s2=sc.nextLine();  // user input using the  Scanner next() method for 2 String variables
-    if(s1.equals(s2)){//String Built-In method to check if the results are the same and display the result 
+class StringEqual{ 
+   static boolean isEqual(String s1,String s2){
+        if(s1.equals(s2)){
+            return true;
+        } 
+        else{
+            return false;
+        }
+    } 
+    static boolean isEqual2(String s1,String s2){
+        if(s1.length()!=s2.length()){
+            return false;
+        } 
+        for(int i=0;i<s1.length();i++){
+            if(s1.charAt(i)!=s2.charAt(i)){
+                return false;
+            } 
+        }
+        return true;
+    }
+    public static void main(String[] args) { 
+        StringEqual s=new StringEqual(); 
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter string 1"); 
+        String st1=sc.nextLine(); 
+        System.out.println("Enter string2"); 
+        String st2=sc.nextLine(); 
+        boolean value=isEqual(st1,st2); 
+        boolean value2=isEqual2(st1,st2); 
+        if(value==value2){ 
+            System.out.println("Strings are equal");
 
-        System.out.println("They are equal");
-    }
-    else{
-        System.out.println("They are not equal");
-    }
-    sc.close();
+        }
+        else{
+            System.out.println("Strings are not equal");
+        }
 
         
     }
-
-    
 
 }
